93f21ed6eaaf4d82756e839d019fe90c3ff48e92
jballetshofer <ge96fec@mytum.de>
2025-05-09 11:41:55 +0200

    added panda, first test robco

--- src/simple_goal_publisher/setup.py
+++ src/simple_goal_publisher/setup.py
@@ -21,6 +21,7 @@ setup(
     entry_points={
     'console_scripts': [
         'simple_goal_publisher = simple_goal_publisher.simple_goal_publisher:main',
+        'simple_goal_publisher_panda = simple_goal_publisher.simple_goal_publisher_panda:main'
     ],
     },
 )
